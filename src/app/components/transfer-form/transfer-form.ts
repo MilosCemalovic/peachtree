360ff62282a9313a239413d8b7c547e61a2b02bc
Milos Cemalovic <miloscemalovicnew@gmail.com>
2026-02-12 20:16:13 +0100

    Added dialog TransferPreview with functionality inside TransferForm component

--- src/app/components/transfer-form/transfer-form.ts
+++ src/app/components/transfer-form/transfer-form.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common'
 import { Component, input, output } from '@angular/core'
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { TransferTo } from '../../models/transfer'
+import { MatDialog } from '@angular/material/dialog'
+import { TransferData, TransferPreview } from '../transfer-preview/transfer-preview'
 
 @Component({
   selector: 'app-transfer-form',
@@ -22,13 +24,31 @@ export class TransferForm {
     amount: new FormControl('', [Validators.required, Validators.min(0.01), Validators.max(500)])
   })
 
+  constructor(private dialog: MatDialog) {}
+
   onSubmit () {
     if (this.transferForm.valid) {
       const amount = Number(this.transferForm.value.amount)
       const toAccount = this.transferForm.value.toAccount!
 
-      this.transferComplete.emit({ toAccount, amount })
-      this.transferForm.reset({ toAccount: '', amount: '' })
+      const dialogData: TransferData = {
+        fromAccount: `FreeChecking(4692) - $${ this.currentBalance().toFixed(2) }`,
+        toAccount,
+        amount
+      }
+
+      const dialogRef = this.dialog.open(TransferPreview, {
+        width: '500px',
+        maxWidth: '95vw',
+        data: dialogData
+      })
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result === 'confirm') {
+          this.transferComplete.emit({ toAccount, amount })
+          this.transferForm.reset({ toAccount: '', amount: '' })
+        }
+      })
     }
   }
 }
